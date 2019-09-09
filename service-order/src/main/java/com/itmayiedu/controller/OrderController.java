488c39263a43fff13a6ddbfdf91fcc472b1350e3
yuhuijuan1 <yuhuijuan1@example.com>
2019-09-10 02:14:45 +0800

    12

--- service-order/src/main/java/com/itmayiedu/controller/OrderController.java
+++ service-order/src/main/java/com/itmayiedu/controller/OrderController.java
@@ -18,6 +18,7 @@ public class OrderController {
 		System.out.println("订单服务开始调用会员服务");
 		return orderMemberService.getOrderUserAll();
 		System.out.println("1111111111111111");
+		System.out.println("222222222222222222222222");
 
 	}
 
