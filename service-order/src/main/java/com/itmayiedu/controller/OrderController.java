fa1e1c15bc5a7dad828e159a29aa9a4f5b671765
yuhuijuan1 <yuhuijuan1@example.com>
2019-09-10 02:28:08 +0800

    55

--- service-order/src/main/java/com/itmayiedu/controller/OrderController.java
+++ service-order/src/main/java/com/itmayiedu/controller/OrderController.java
@@ -20,6 +20,7 @@ public class OrderController {
 		System.out.println("1111111111111111");
 		System.out.println("222222222222222222222222");
 		System.out.println("333333333333333333333");
+        System.out.println("5555555555555555555555");
 
 	}
 
